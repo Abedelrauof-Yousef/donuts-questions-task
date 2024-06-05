3609f0acc34e0e3b46c0dbee68ba72c71f5160c1
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-05 23:57:54 +0300

    changes

---
+++ app.js
@@ -0,0 +1,72 @@
+// Ex1
+// var userName = prompt("What is your name ?");
+
+// var gender = prompt("What is your Gender ?");
+
+// if (gender == "Male" || gender == "male"){
+//     alert("Hello Mr " + userName +"!");
+// } else if (gender == "Female" || gender == "female"){
+//     alert("Hello Mrs " + userName + "!");
+// } else {
+//     alert("Welcome Stranger!")
+// }
+
+// alert("Mr " + userName + ", What would you like to order, Donuts, Ice Cream, Coffe or Bakery")
+
+// var order = prompt("What is your order")
+
+// if(order == "Donuts" || order == "donuts"){
+//     alert(userName + " You've ordered donuts")
+// } else if (order == "Ice Cream" || order == "ice cream"){
+//     alert(userName + " You've ordered Ice Cream")
+// } else if (order == "Coffee" || order == "coffee"){
+//     alert(userName + " You've ordered Coffe")
+// } else if (order == "Bakery" || order == "bakery"){
+//     alert(userName + " You've ordered Bakery")
+// }
+
+// alert("Your order is being prepared")
+
+// console.log(userName + " you've ordered " + order + " Thank you for using our application");
+
+
+
+
+// Ex2
+//   let userGender = prompt("Please enter your gender (male/female):");
+// while (userGender !== "male" && userGender !== "female") {
+//   userGender = prompt("Please enter your gender (male/female):");
+//   if (userGender === "male" || userGender === "female") {
+//     break;
+//   }
+// }
+
+// alert("Thank you! Your gender is recorded as " + userGender);
+
+// let arrayData = [userName, gender, order];
+// for (let i = 0; i < arrayData.length; i++){
+
+// console.log(arrayData[i]);
+// }
+
+
+
+
+// Ex3
+// function chooseGender() {
+//   let userGender = prompt("Please enter your gender (male/female):");
+//   while (userGender !== "male" && userGender !== "female") {
+//     userGender = prompt("Please enter your gender (male/female):");
+//     if (userGender === "male" || userGender === "female") {
+//       break;
+//     }
+//   }
+// }
+// chooseGender();
+
+
+
+
+
+
+
