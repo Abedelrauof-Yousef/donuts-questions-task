e6de7c5fba9cfc2cb82cebfb4b7585da4e06beb3
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-09 18:18:44 +0300

    dom task

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 // Ex1
-// var userName = prompt("What is your name ?");
+var userName = prompt("What is your name ?");
 
-// var gender = prompt("What is your Gender ?");
+var gender = prompt("What is your Gender ?");
 
-// if (gender == "Male" || gender == "male"){
-//     alert("Hello Mr " + userName +"!");
-// } else if (gender == "Female" || gender == "female"){
-//     alert("Hello Mrs " + userName + "!");
-// } else {
-//     alert("Welcome Stranger!")
-// }
+if (gender == "Male" || gender == "male"){
+    alert("Hello Mr " + userName +"!");
+} else if (gender == "Female" || gender == "female"){
+    alert("Hello Mrs " + userName + "!");
+} else {
+    alert("Welcome Stranger!")
+}
 
-// alert("Mr " + userName + ", What would you like to order, Donuts, Ice Cream, Coffe or Bakery")
+alert("Mr " + userName + ", What would you like to order, Donuts, Ice Cream, Coffe or Bakery")
 
-// var order = prompt("What is your order")
+var order = prompt("What is your order")
 
-// if(order == "Donuts" || order == "donuts"){
-//     alert(userName + " You've ordered donuts")
-// } else if (order == "Ice Cream" || order == "ice cream"){
-//     alert(userName + " You've ordered Ice Cream")
-// } else if (order == "Coffee" || order == "coffee"){
-//     alert(userName + " You've ordered Coffe")
-// } else if (order == "Bakery" || order == "bakery"){
-//     alert(userName + " You've ordered Bakery")
-// }
+if(order == "Donuts" || order == "donuts"){
+    alert(userName + " You've ordered donuts")
+} else if (order == "Ice Cream" || order == "ice cream"){
+    alert(userName + " You've ordered Ice Cream")
+} else if (order == "Coffee" || order == "coffee"){
+    alert(userName + " You've ordered Coffe")
+} else if (order == "Bakery" || order == "bakery"){
+    alert(userName + " You've ordered Bakery")
+}
 
-// alert("Your order is being prepared")
+alert("Your order is being prepared")
 
-// console.log(userName + " you've ordered " + order + " Thank you for using our application");
+console.log(userName + " you've ordered " + order + " Thank you for using our application");
 
 
 
@@ -68,19 +68,19 @@
 
 
 // Ex4
-// let user_Name = document.getElementById("userName");
-// let userInformation = document.getElementById("userInformation");
-// let userData = userInformation.getElementsByTagName("li");
-// let heroDis = document.createElement("p");
-// let userInformationSection = document.getElementById("userInformationSection");
-// user_Name.innerText = ("Order maker is: " + userName) ;
-// userData[0].innerText = ("Gender of the order maker is: " + gender);
-// userData[1].innerText = ("The order  you've ordered is: " + order);
-// heroDis.innerText = "we believe every day is a little sweeter with a donut in hand. Our artisanal donuts are handcrafted with love, using the finest ingredients to create a melt-in-your-mouth experience. From classic flavors to innovative creations, there's a perfect treat waiting for you. Join us for a bite of happiness!"
-// heroDis.style.textAlign = "center"
-// heroDis.style.fontSize = "larger"
-// heroDis.style.marginTop = "50px"
-// userInformationSection.appendChild(heroDis);
+let user_Name = document.getElementById("userName");
+let userInformation = document.getElementById("userInformation");
+let userData = userInformation.getElementsByTagName("li");
+let heroDis = document.createElement("p");
+let userInformationSection = document.getElementById("userInformationSection");
+user_Name.innerText = ("Order maker is: " + userName) ;
+userData[0].innerText = ("Gender of the order maker is: " + gender);
+userData[1].innerText = ("The order  you've ordered is: " + order);
+heroDis.innerText = "we believe every day is a little sweeter with a donut in hand. Our artisanal donuts are handcrafted with love, using the finest ingredients to create a melt-in-your-mouth experience. From classic flavors to innovative creations, there's a perfect treat waiting for you. Join us for a bite of happiness!"
+heroDis.style.textAlign = "center"
+heroDis.style.fontSize = "larger"
+heroDis.style.marginTop = "50px"
+userInformationSection.appendChild(heroDis);
 
 
 
