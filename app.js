751b885c607802dea8c5ee9934ba1c961d4ee457
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-09 18:11:29 +0300

    dom

--- app.js
+++ app.js
@@ -67,6 +67,24 @@
 
 
 
+// Ex4
+// let user_Name = document.getElementById("userName");
+// let userInformation = document.getElementById("userInformation");
+// let userData = userInformation.getElementsByTagName("li");
+// let heroDis = document.createElement("p");
+// let userInformationSection = document.getElementById("userInformationSection");
+// user_Name.innerText = ("Order maker is: " + userName) ;
+// userData[0].innerText = ("Gender of the order maker is: " + gender);
+// userData[1].innerText = ("The order  you've ordered is: " + order);
+// heroDis.innerText = "we believe every day is a little sweeter with a donut in hand. Our artisanal donuts are handcrafted with love, using the finest ingredients to create a melt-in-your-mouth experience. From classic flavors to innovative creations, there's a perfect treat waiting for you. Join us for a bite of happiness!"
+// heroDis.style.textAlign = "center"
+// heroDis.style.fontSize = "larger"
+// heroDis.style.marginTop = "50px"
+// userInformationSection.appendChild(heroDis);
+
+
+
+
 
 
 
